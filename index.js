462d54d670c5a2bca7ed881b073bfb26a8a746e6
LoliKillers <monkidirufi157@gmail.com>
2021-10-15 10:16:34 +0700

    Update 2

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
-const { igstalk } = require('./lib/function')
+const {
+  IgStalk,
+  IgStory,
+  IgDl,
+  Pinterest,
+  TiktokDl
+} = require('./lib/function')
+const { Halah } = require('./lib/halah')
+const { Hilih } = require('./lib/hilih')
+const { Huluh } = require('./lib/huluh')
+const { Heleh } = require('./lib/heleh')
+const { Holoh } = require('./lib/holoh')
+const { HttpHeaders } = require('./lib/headers')
+const { Ip } = require('./lib/ip')
+const { IpLookup } = require('./lib/iplookup')
+const { JooxPlay } = require('./lib/jooxplay')
+const { Proxy } = require('./lib/proxy')
+const {
+  YtdlMp3,
+  YtdlMp4,
+  YtPlayMp3,
+  YtPlayMp4,
+  YtSearch
+} = require('./lib/yt')
 
-module.exports.igstalk = igstalk
+module.exports.IgStalk = IgStalk
+module.exports.IgStory = IgStory
+module.exports.IgDl = IgDl
+module.exports.Pinterest = Pinterest
+module.exports.TiktokDl = TiktokDl
+module.exports.Halah = Halah
+module.exports.Hilih = Hilih
+module.exports.Huluh = Huluh
+module.exports.Heleh = Heleh
+module.exports.Holoh = Holoh
+module.exports.Holoh = Holoh
+module.exports.HttpHeaders = HttpHeaders
+module.exports.Ip = Ip
+module.exports.IpLookup = IpLookup
+module.exports.Proxy = Proxy
+module.exports.YtdlMp3 = YtdlMp3
+module.exports.YtdlMp4 = YtdlMp4
+module.exports.YtPlayMp3 = YtPlayMp3
+module.exports.YtPlayMp4 = YtPlayMp4
+module.exports.YtSearch = YtSearch
