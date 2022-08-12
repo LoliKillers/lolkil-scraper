adaf1e85896b7950730d0bc691de20af247cb31d
LoliKillers <monkidirufi157@gmail.com>
2022-08-13 01:57:48 +0700

    fix error

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 "use strict";
-require('rootpath')();
-module.exports.anime = require('lib/anime/index.js');
-module.exports.download = require('lib/download/index.js');
-module.exports.convert = require('lib/convert/index.js');
-module.exports.info = require('lib/info/index.js');
+module.exports.anime = require('./lib/anime/index.js');
+module.exports.download = require('./lib/download/index.js');
+module.exports.convert = require('./lib/convert/index.js');
+module.exports.info = require('./lib/info/index.js');
