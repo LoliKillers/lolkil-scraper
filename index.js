595fe3e0293e64401f03206d8eecc47d2d29e538
LoliKillers <monkidirufi157@gmail.com>
2022-08-15 15:48:03 +0700

    fix error

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ module.exports.anime = require('./lib/anime/index.js');
 module.exports.download = require('./lib/download/index.js');
 module.exports.convert = require('./lib/convert/index.js');
 module.exports.info = require('./lib/info/index.js');
+module.exports.image = require('./lib/image/index.js');
+module.exports.stalk = require('./lib/stalk/index.js');
+module.exports.search = require('./lib/search/index.js');
